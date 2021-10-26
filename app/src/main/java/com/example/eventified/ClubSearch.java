b343aa8eca23fe226c4c275aa8c89948f300cfc6
Jaspann <wiparker2020@gmail.com>
2021-10-26 14:29:39 -0400

    Set up navigation drawer

--- app/src/main/java/com/example/eventified/ClubSearch.java
+++ app/src/main/java/com/example/eventified/ClubSearch.java
@@ -1,24 +1,55 @@
 package com.example.eventified;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ClubSearch extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_search);
-        configureHomeButton();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.navigation);
+
+        toggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    public void configureHomeButton()
+    @Override
+    public void onBackPressed()
     {
-        ImageButton homeButton = (ImageButton) findViewById(R.id.HomeButton);
-        homeButton.setOnClickListener(v -> finish());
+        if(drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
+
 }
